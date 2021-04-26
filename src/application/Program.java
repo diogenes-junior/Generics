4e9ea6674f544384f185da9fb401245dfeca269f
Diogenes <diogenes.junior@gmail.com>
2021-04-26 16:27:30 -0300

    Applying Generics

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,18 +9,30 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		PrintService ps = new PrintService();
+		PrintService<Integer> ps = new PrintService<>();
+		PrintService<String> ps2 = new PrintService<>();
 		
 		System.out.println("How many values? ");
 		int n = input.nextInt();
 		
 		for(int i=1; i<=n; i++) {
-			int number = input.nextInt();
-			ps.addValue(number);
+			int value = input.nextInt();
+			ps.addValue(value);
+		}
+		
+		System.out.println("How many values string? ");
+		n = input.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			String value = input.next();
+			ps2.addValue(value);
 		}
 		
 		ps.print();
 		System.out.println("First: " + ps.first());
+		System.out.println();
+		ps2.print();
+		System.out.println("First: " + ps2.first());
 		
 		input.close();
 	}
